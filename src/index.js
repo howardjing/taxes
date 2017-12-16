54e6cc706eb77da1d713924adf371416063b6b43
Howard Jing <howard.jing@gmail.com>
2017-12-15 23:54:14 -0500

    add flow

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-console.log("hello world");
+// @flow
+
+const body = document.querySelector('body');
+const div = document.createElement('div');
+div.innerHTML = "Hello";
+
+if (body) {
+  body.appendChild(div);
+}
+
+const brackets = [
+  { cap: 9325, amount: 0.1 },
+  { cap: 37950, amount: 0.15 },
+  { cap: 91900, amount: 0.25 },
+  { cap: 191650, amount: 0.28 },
+  { cap: 416700, amount: 0.33 },
+  { cap: 418400, amount: 0.35 },
+  { cap: Infinity, amount: 0.396 },
+];
