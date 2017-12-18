f6694b494ad9dcace1947277192a3e849cd0242f
Howard Jing <howard.jing@gmail.com>
2017-12-17 23:08:36 -0500

    basic visualization of taxes

--- src/index.js
+++ src/index.js
@@ -11,13 +11,13 @@ class Bracket {
   // tax rate within this bracket
   percent: number;
   // max amount of taxes for this bracket
-  maxAmount: number;
+  taxes: number;
 
   constructor(cap, length, percent) {
     this.cap = cap;
     this.length = length;
     this.percent = percent;
-    this.maxAmount = percentOf(length, percent);
+    this.taxes = percentOf(length, percent);
   }
 }
 
@@ -45,51 +45,90 @@ const BRACKETS_2017: Bracket[] = buildBrackets([
   [191650, 0.28],
   [416700, 0.33],
   [418400, 0.35],
-  // [Infinity, 0.396],
+  [Infinity, 0.396],
 ]);
 
 /**
  * assumes brackets is sorted in ascending order by cap
  */
 const marginalTaxAmount = (allBrackets: Bracket[], income: number): number => {
-  return sum(marginalTaxAmounts(allBrackets, income));
+  return sum(marginalTaxAmounts(allBrackets, income).map(bracket => bracket.taxes));
 };
 
-const marginalTaxAmounts = (allBrackets: Bracket[], income: number): number[] => {
+const marginalTaxAmounts = (allBrackets: Bracket[], income: number): Bracket[] => {
   const brackets = relevantBrackets(allBrackets, income);
 
   // no relevant brackets means no taxes whooooo
   if (brackets.length === 0) { return []; }
 
   // if there's only one bracket, just calculate tax rate directly
-  if (brackets.length === 1) { return [percentOf(income, brackets[0].percent)]; }
+  if (brackets.length === 1) { return [new Bracket(income, income, brackets[0].percent)]; }
 
   // when > 2 brackets, things matter
   const tail = brackets.pop();
   const lastFullBracket = brackets[brackets.length - 1].cap;
   const margin = income - lastFullBracket;
-  return brackets.map(bracket => bracket.maxAmount).concat([percentOf(margin, tail.percent)]);
+  return brackets.concat([new Bracket(income, margin, tail.percent)]);
 }
 
 const relevantBrackets = (allBrackets: Bracket[], income: number): Bracket[] => {
-  const index = allBrackets.findIndex(bracket => bracket.cap > income);
+  const index = allBrackets.findIndex(bracket => bracket.cap > income)
   return allBrackets.slice(0, index + 1);
 }
 
 const sum = (xs: number[]): number => xs.reduce((accum, x) => accum + x, 0);
 
+// https://stackoverflow.com/questions/149055/how-can-i-format-numbers-as-dollars-currency-string-in-javascript
+const dollars = (n: number): string => `$${n.toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g, '$1,')}`
 // =============
 
-const brackets = relevantBrackets(BRACKETS_2017, 10000);
-const max = brackets[brackets.length - 1].cap;
+const income = 100000;
+const brackets = marginalTaxAmounts(BRACKETS_2017, income);
+const max = income;
 
-select("body")
+const body = document.querySelector('body');
+if (!body) { throw new Error('huh oh' )}
+
+const header = document.createElement('h1');
+header.innerHTML = `Income: ${dollars(income)}`;
+
+const container = document.createElement('div');
+
+const taxed = marginalTaxAmount(BRACKETS_2017, income);
+const rate = taxed / income;
+const taxes = document.createElement('div');
+taxes.innerHTML = `Taxes: ${dollars(taxed)} | Tax rate: ${(rate * 100).toFixed(3)}%`;
+
+
+body.appendChild(header);
+body.appendChild(container);
+body.appendChild(taxes);
+
+const wrapper = select(container)
   .selectAll('bracket')
   .data(brackets)
-  .enter().append('div')
-    .style('width', bracket => `${bracket.length * 100 / max}%`)
-    .style('display', 'inline-block')
-    .text(d => d.length);
+  .enter()
+    .append('div')
+      .style('width', (bracket) => `${(bracket.length / max) * 90}%`)
+      .style('height', '60px')
+      .style('display', 'inline-block')
+      .style('background-color', 'green')
+      .style('margin', '0 10px 0 0')
 
-console.log(marginalTaxAmounts(BRACKETS_2017, 100000));
-console.log(marginalTaxAmount(BRACKETS_2017, 100000));
+wrapper
+    .append('span')
+    .style('float', 'right')
+    .text( (bracket) => dollars(bracket.length))
+
+wrapper
+    .append('span')
+    .style('height', '100%')
+    .style('background-color', 'purple')
+    .style('width', (bracket) => `${bracket.percent * 100}%`)
+    .style('float', 'left')
+    .text( (bracket) => dollars(bracket.taxes))
+
+console.log(relevantBrackets(BRACKETS_2017, income));
+console.log(marginalTaxAmounts(BRACKETS_2017, income));
+console.log(zip(relevantBrackets(BRACKETS_2017, income), marginalTaxAmounts(BRACKETS_2017, income)))
+console.log(marginalTaxAmount(BRACKETS_2017, income));
