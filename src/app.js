9ed22ae203d6f8d4b3e8be39c2f27d4fdf942170
Howard Jing <howard.jing@gmail.com>
2017-12-22 01:36:41 -0500

    add data for joint / separate / head brackets

--- src/app.js
+++ src/app.js
@@ -15,15 +15,46 @@ import type { Bracket, Currency } from './compute-taxes';
 import TaxBrackets from './tax-brackets';
 import dollars from './format-dollars';
 
-const BRACKETS_2017: Bracket[] = buildBrackets([
-  [9325, 0.1],
-  [37950, 0.15],
-  [91900, 0.25],
-  [191650, 0.28],
-  [416700, 0.33],
-  [418400, 0.35],
-  [Infinity, 0.396],
-]);
+type FilingOption = 'single' | 'joint' | 'head' | 'separate';
+
+const brackets2017: { [FilingOption]: Bracket[] } = {
+  single: buildBrackets([
+    [9325, 0.1],
+    [37950, 0.15],
+    [91900, 0.25],
+    [191650, 0.28],
+    [416700, 0.33],
+    [418400, 0.35],
+    [Infinity, 0.396],
+  ]),
+  joint: buildBrackets([
+    [18650, 0.1],
+    [75900, 0.15],
+    [153100, 0.25],
+    [233350, 0.28],
+    [416700, 0.33],
+    [470700, 0.35],
+    [Infinity, 0.396],
+  ]),
+  separate: buildBrackets([
+    [9325, 0.1],
+    [37950, 0.15],
+    [76550, 0.25],
+    [116675, 0.28],
+    [208350, 0.33],
+    [235350, 0.35],
+    [Infinity, 0.396],
+  ]),
+  head: buildBrackets([
+    [13350, 0.1],
+    [50800, 0.15],
+    [131200, 0.25],
+    [212500, 0.28],
+    [416700, 0.33],
+    [444500, 0.35],
+    [Infinity, 0.396],
+  ]),
+};
 
 // TODO: salarys should by dynamic I guess -- right now
 // if user enters an income more than 1 million, it won't be
@@ -34,15 +65,23 @@ for (let i=1000; i <= 1000000; i += 1000) {
   salarys.push(i);
 }
 
-const taxIntervals = federalIncomeTaxBrackets(BRACKETS_2017, MAX).map(bracket => [
-  { x: bracket.min, y: bracket.rate },
-  { x: bracket.max, y: bracket.rate },
-]);
+const filingOptions = Object.keys(brackets2017);
 
-const computedTaxRates = salarys.map(sampleIncome => {
-  const taxes = federalIncomeTax(BRACKETS_2017, sampleIncome);
-  const rate = taxes / sampleIncome;
-  return { x: sampleIncome, y: rate };
+const taxIntervals = {};
+filingOptions.forEach(option => {
+  taxIntervals[option] = federalIncomeTaxBrackets(brackets2017[option], MAX).map(bracket => [
+    { x: bracket.min, y: bracket.rate },
+    { x: bracket.max, y: bracket.rate },
+  ]);
+});
+
+const computedTaxRates = {};
+filingOptions.forEach(option => {
+  computedTaxRates[option] = salarys.map(sampleIncome => {
+    const taxes = federalIncomeTax(brackets2017[option], sampleIncome);
+    const rate = taxes / sampleIncome;
+    return { x: sampleIncome, y: rate };
+  });
 });
 
 const Container = styled.div`
@@ -58,11 +97,14 @@ type Props = {};
 
 type State = {
   income: Currency,
+  filingOption: FilingOption,
 };
+
 
 class App extends React.Component<Props, State> {
   state = {
     income: 100000,
+    filingOption: 'single',
   };
 
   handleIncomeChange = (e: SyntheticEvent<HTMLInputElement>) => {
@@ -75,21 +117,45 @@ class App extends React.Component<Props, State> {
     }));
   };
 
+  handleFilingOptionChange = (e: SyntheticEvent<HTMLInputElement>) => {
+    const { value } = e.currentTarget;
+    this.setState(() => ({
+      // TODO: figure out proper way to say value is a FilingOption
+      filingOption: (value: any),
+    }));
+  }
+
   render() {
-    const { income } = this.state;
-    const taxes = federalIncomeTax(BRACKETS_2017, income);
+    const { income, filingOption } = this.state;
+    const taxes = federalIncomeTax(brackets2017[filingOption], income);
     const rate = taxes / income;
     return (
       <Container>
         <div>
-          gross income:
-          <input
-            type="number"
-            value={income}
-            onChange={this.handleIncomeChange}
-          />
+          <div>
+            gross income:
+            <input
+              type="number"
+              value={income}
+              onChange={this.handleIncomeChange}
+            />
+          </div>
+          <div>
+            filing option:
+            {filingOptions.map(option => (
+              <label key={option}>
+                <input
+                  type="radio"
+                  value={option}
+                  checked={option === filingOption}
+                  onChange={this.handleFilingOptionChange}
+                />
+                {option}
+              </label>
+            ))}
+          </div>
         </div>
-        <TaxBrackets brackets={BRACKETS_2017} income={income} />
+        <TaxBrackets brackets={brackets2017[filingOption]} income={income} />
         <GraphContainer>
           <FlexibleXYPlot
             xDomain={[0, MAX]}
@@ -100,8 +166,8 @@ class App extends React.Component<Props, State> {
             <XAxis title="gross income" />
             <YAxis title="tax rate" />
             <MarkSeries data={[{ x: income, y: rate }]}/>
-            <LineSeries data={computedTaxRates} />
-            {taxIntervals.map((data, i) =>
+            <LineSeries data={computedTaxRates[filingOption]} />
+            {taxIntervals[filingOption].map((data, i) =>
               <LineSeries key={i} data={data} strokeStyle="dashed" />
             )}
           </FlexibleXYPlot>
